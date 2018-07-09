1719d5cc52d4e7fcd1900807d5f0b18b827da8d7
Jasna Urbancic <jasna1994@gmail.com>
2018-07-09 10:28:15 +0200

    Added POST request for recommendation

--- src/server/recsys/routes/recommendations.js
+++ src/server/recsys/routes/recommendations.js
@@ -44,6 +44,35 @@ module.exports = function (pg, logger) {
         return res.send(recommendations);
 
     });
+    
+    // GET recommendation based on query
+    router.post('/recommend/content', (req, res) => {
+        logger.info('client requested for recommendation',
+            logger.formatRequest(req)
+        );
+
+        // get the query parameters
+        let query = req.body;
+
+        if (Object.keys(query).length === 0) {
+            // no query parameters were given
+            let errorMessage = 'user did not provide any of the query parameters: text, url';
+            logger.warn('warning [query_parameters]: client requested for recommendation failed',
+                logger.formatRequest(req, { error: errorMessage })
+            );
+            // send error response
+            return res.send({ error: errorMessage });
+        }
+
+        // get the recommended material
+        let recommendations = x5recommend.recommendContent(query);
+
+        // log the recommendation success
+        logger.info('client requested for recommendation successful');
+        // send the recommendations to the user
+        return res.send(recommendations);
+
+    });
 
     return router;
 };
