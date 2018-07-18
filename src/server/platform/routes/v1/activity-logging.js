be16cb5af35fddf9f4a5b29f3c1c4182eadf71ed
Erik Novak <novak.erik@gmail.com>
2018-07-18 14:17:00 +0200

    Fix serving x5gon library

--- src/server/platform/routes/v1/activity-logging.js
+++ src/server/platform/routes/v1/activity-logging.js
@@ -42,13 +42,7 @@ module.exports = function (pg, logger) {
         // the beacon used to acquire user activity data
         let beaconPath = path.join(__dirname, '../../snippet/images/beacon.png');
         // get query parameters
-        let userParameters = { }; ;
-
-        userParameters.x5gonValidated = decodeURIComponent(req.query.x5gonValidated);
-        userParameters.dt = decodeURIComponent(req.query.dt);
-        userParameters.rq = decodeURIComponent(req.query.rq);
-        userParameters.rf = decodeURIComponent(req.query.rf);
-        userParameters.cid = decodeURIComponent(req.query.cid);
+        let userParameters = req.query;
 
         // validate query schema
         if (!Object.keys(userParameters).length ||
@@ -110,7 +104,7 @@ module.exports = function (pg, logger) {
 
         // get the file name
         let originalUrl = req.originalUrl.split('/');
-        const file = originalUrl[originalUrl.length - 1];
+        const file = originalUrl[originalUrl.length - 1].split('?')[0];
         // create the file path
         const filePath = path.join(__dirname, `../../snippet/global/${version}/${file}`);
 
