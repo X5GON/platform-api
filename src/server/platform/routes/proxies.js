5d792b20476ac3d0a9ca80a8b77069148862f9e2
Erik Novak <novak.erik@gmail.com>
2019-07-01 16:22:25 +0200

    Add quality assurance proxy

--- src/server/platform/routes/proxies.js
+++ src/server/platform/routes/proxies.js
@@ -24,4 +24,13 @@ module.exports = function (app, config) {
         }
     }));
 
+    // redirect to the Recommendation System route
+    app.use('/api/v1/qa', proxy('/api/latest/qa', {
+        target: `http://127.0.0.1:${config.quality.port}`,
+        logProvider: function (provider) {
+            // create logger for sending requests
+            return Logger.createInstance(`proxy`, 'info', 'platform', config.environment !== 'prod');
+        }
+    }));
+
 };
