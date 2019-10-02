c55cc106a655349c408db8302f98c4059fa5db0d
Erik Novak <novak.erik@gmail.com>
2019-10-02 16:14:08 +0200

    Remove public files

--- src/server/platform/public/styles-4c7488e1cde7ea83eb2f.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{226:function(n,o,w){},236:function(n,o,w){},237:function(n,o,w){}}]);
-//# sourceMappingURL=styles-4c7488e1cde7ea83eb2f.js.map
